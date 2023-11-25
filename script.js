3b965d0aaeec2bf91f7144f91f1cfb80f7ffc60c
kervines <kervinzero@gmail.com>
2023-11-25 00:11:25 -0300

    update project

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const joinButton = document.querySelector('.button');
 const popUp = document.querySelector('.pop-up');
 const bodyEl = document.querySelector('body');
-const containerEl = document.querySelector('.container');
+const mainEl = document.querySelector('.main');
 const closeButton = document.querySelector('.close');
 
 joinButton.addEventListener('click', () => {
@@ -9,7 +9,7 @@ joinButton.addEventListener('click', () => {
     popUp.classList.remove('move-down');
   popUp.classList.add('move-up');
   popUp.style.pointerEvents = 'auto';
-  containerEl.classList.add('blur-container');
+  mainEl.classList.add('blur-container');
   bodyEl.classList.add('blur');
 });
 
@@ -17,6 +17,6 @@ closeButton.addEventListener('click', () => {
   popUp.classList.remove('move-up');
   popUp.classList.add('move-down');
   popUp.style.pointerEvents = 'none';
-  containerEl.classList.remove('blur-container');
+  mainEl.classList.remove('blur-container');
   bodyEl.classList.remove('blur');
 });
